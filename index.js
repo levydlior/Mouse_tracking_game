64fa5a637d4b0f69df686ff0ad815e0fdaa7816a
Lior <levydlior@gmail.com>
2022-03-25 15:44:07 -0600

    moving target dom mechanics

--- index.js
+++ index.js
@@ -5,7 +5,16 @@ const target = document.querySelector("#target")
 let isRunning = false;
 
 
+const moveSideways = (min, max)=>{ 
+    let number = Math.random()*(-59.4 - 0)
+    return number
+}
+const moveUpAndDown = ()=>{ 
+    let number = Math.random()*(-34.8 - 0)
+    return number
+}
+
 target.addEventListener("click", () =>{
-  
-    
+   target.style.right= moveSideways() +"rem"
+   target.style.bottom= moveUpAndDown() +"rem"
 })
