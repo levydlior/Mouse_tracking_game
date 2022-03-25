6950e2f776c6d3e001519e2de5dad2b90c7bd738
Lior <levydlior@gmail.com>
2022-03-25 15:11:56 -0600

    add dom

---
+++ index.js
@@ -0,0 +1,11 @@
+const timeDisplay= document.querySelector("#timer")
+const scoreDisplay= document.querySelector("#score")
+const target = document.querySelector("#target")
+
+let isRunning = false;
+
+
+target.addEventListener("click", () =>{
+  
+    
+})
