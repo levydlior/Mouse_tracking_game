f3d0b8a6d698c78c34154ec4f9a7b9ef0d1d8793
Lior <levydlior@gmail.com>
2022-03-25 15:47:36 -0600

    clean up js file

--- index.js
+++ index.js
@@ -5,16 +5,24 @@ const target = document.querySelector("#target")
 let isRunning = false;
 
 
-const moveSideways = (min, max)=>{ 
-    let number = Math.random()*(-59.4 - 0)
-    return number
-}
-const moveUpAndDown = ()=>{ 
-    let number = Math.random()*(-34.8 - 0)
+const targetLocationGenerator = (min, max)=>{ 
+    let number = Math.random()*(min - max)
     return number
 }
 
+const movingTarget = () => {
+    target.style.right= targetLocationGenerator(-59.4, - 0) +"rem"
+    target.style.bottom= targetLocationGenerator(-34.8, - 0) +"rem"
+}
+
+
+
+
 target.addEventListener("click", () =>{
-   target.style.right= moveSideways() +"rem"
-   target.style.bottom= moveUpAndDown() +"rem"
+    isRunning = true;
+    if (isRunning === true){
+        movingTarget()
+    }else{
+        null
+    }
 })
