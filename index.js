4be050ae683d22b29849b17d41a249d484d71c42
Lior <levydlior@gmail.com>
2022-03-26 16:02:59 -0600

    add scaling level, add game stop when timer is 0

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const runGame = () => {
         isRunning = true
         startGame.innerText = "Restart"
         countDown()
-}
+    }
 }
 
 
@@ -24,33 +24,44 @@ const targetLocationGenerator = (min, max)=>{
     let number = Math.random()*(min - max)
     return number
 }
-
 const movingTarget = () => {
     target.style.right= targetLocationGenerator(-59.4, - 0) +"rem"
     target.style.bottom= targetLocationGenerator(-34.8, - 0) +"rem"
 }
 
+const increaseDifficulty = ()=> {
+    if (scoreDisplay.innerText < 5){
+        timeDisplay.innerText = 10
+    }else if (scoreDisplay.innerText < 10){
+        timeDisplay.innerText = 8
+    }else if (scoreDisplay.innerText < 15){
+        timeDisplay.innerText = 5
+    }else if (scoreDisplay.innerText < 20){
+        timeDisplay.innerText = 3
+    }else if (scoreDisplay.innerText < 25){
+        timeDisplay.innerText = 1
+    }
+ }
+
+
 const countDown = () => {
-    
-    setInterval(() =>{
+    const countDownTimer = setInterval(() =>{
         if (timeDisplay.innerText > 0){
             timeDisplay.innerText--
-        }
-    }, 1000)
-}
-
-const restart = () => {
-    if (timeDisplay.innerText == 0){
-        timeDisplay.innerText = 10
-        clearInterval(countDown)
+        }else if (timeDisplay.innerText === "0"){
+            isRunning = false
+            console.log("gameover", isRunning)
+            clearInterval(countDownTimer)
     }
+    }, 1000);
 }
 
 
 target.addEventListener("click", () =>{
     if (isRunning){
-        movingTarget()
+        // movingTarget()
         scoreDisplay.innerText ++
-    }
+        increaseDifficulty()
+        }
 })
 
