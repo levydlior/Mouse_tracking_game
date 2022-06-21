659b6f3718c256d0cf5ae0ce94ab4c0a3d7ca650
Lior <levydlior@gmail.com>
2022-06-21 09:40:36 -0600

    disabled button when game runs

--- index.js
+++ index.js
@@ -1,87 +1,85 @@
-const timeDisplay= document.querySelector("#timer")
-const scoreDisplay= document.querySelector("#score")
-const target = document.querySelector("#target")
-const startGame = document.querySelector("#start")
-
+const timeDisplay = document.querySelector("#timer");
+const scoreDisplay = document.querySelector("#score");
+const target = document.querySelector("#target");
+const startGame = document.querySelector("#start");
+const startButton = document.querySelector('#start')
 
 let isRunning = false;
 
-const startButtonActivation = ()=> {
-    if (!isRunning && startGame.innerText === "Start"){
-        // startGame.disabled = true
-        startGame.innerText = "Restart"
-        isRunning = true
-    }else if (!isRunning && startGame.innerText === "Restart" && timeDisplay.innerText === "0"){
-        console.log("Restarting")
-        timeDisplay.innerText = 10
-        isRunning = true
-        scoreDisplay.innerText = 0
-        target.style.right = "-30rem";
-        target.style.bottom = "-19rem";
-    }
-}
+const startButtonActivation = () => {
+  if (!isRunning && startGame.innerText === "Start") {
+    startGame.innerText = "Restart";
+    isRunning = true;
+    startGame.disabled = true
+  } else if (
+    !isRunning &&
+    startGame.innerText === "Restart" &&
+    timeDisplay.innerText === "0"
+  ) {
+    timeDisplay.innerText = 10;
+    isRunning = true;
+    scoreDisplay.innerText = 0;
+    target.style.right = "-30rem";
+    target.style.bottom = "-19rem";
+    startGame.disabled = true
+  }
+};
 
 const runGame = () => {
-    if (!isRunning){
-        startButtonActivation()
-        countDown()
-        
-    }
-}
+  if (!isRunning) {
+    startButtonActivation();
+    countDown();
+  }
+};
 
+startGame.addEventListener("click", () => {
+  runGame();
+});
 
-startGame.addEventListener('click', ()=>{
-    runGame()  
-})
-
-
-const targetLocationGenerator = (min, max)=>{ 
-    let number = Math.random()*(min - max)
-    return number
-}
+const targetLocationGenerator = (min, max) => {
+  let number = Math.random() * (min - max);
+  return number;
+};
 const movingTarget = () => {
-    target.style.right= targetLocationGenerator(-59.4, - 0) +"rem"
-    target.style.bottom= targetLocationGenerator(-34.8, - 0) +"rem"
-}
+  target.style.right = targetLocationGenerator(-59.4, -0) + "rem";
+  target.style.bottom = targetLocationGenerator(-34.8, -0) + "rem";
+};
 
-const increaseDifficulty = ()=> {
-    if (scoreDisplay.innerText < 5){
-        timeDisplay.innerText = 10
-        target.style.transition =  "2s"
-    }else if (scoreDisplay.innerText < 10){
-        timeDisplay.innerText = 8
-        target.style.transition =  "1.5s"
-    }else if (scoreDisplay.innerText < 15){
-        timeDisplay.innerText = 5
-        target.style.transition =  "1.3s"
-    }else if (scoreDisplay.innerText < 20){
-        timeDisplay.innerText = 3
-        target.style.transition =  "1s"
-    }else if (scoreDisplay.innerText < 25){
-        timeDisplay.innerText = 1
-        target.style.transition =  "0s"
-    }
- }
-
+const increaseDifficulty = () => {
+  if (scoreDisplay.innerText < 5) {
+    timeDisplay.innerText = 10;
+    target.style.transition = "2s";
+  } else if (scoreDisplay.innerText < 10) {
+    timeDisplay.innerText = 8;
+    target.style.transition = "1.5s";
+  } else if (scoreDisplay.innerText < 15) {
+    timeDisplay.innerText = 5;
+    target.style.transition = "1.3s";
+  } else if (scoreDisplay.innerText < 20) {
+    timeDisplay.innerText = 3;
+    target.style.transition = "1s";
+  } else if (scoreDisplay.innerText < 25) {
+    timeDisplay.innerText = 1;
+    target.style.transition = "0s";
+  }
+};
 
 const countDown = () => {
-    const countDownTimer = setInterval(() =>{
-        if (timeDisplay.innerText > 0){
-            timeDisplay.innerText--
-        }else if (timeDisplay.innerText === "0"){
-            isRunning = false
-            console.log("gameover", isRunning)
-            clearInterval(countDownTimer)
+  const countDownTimer = setInterval(() => {
+    if (timeDisplay.innerText > 0) {
+      timeDisplay.innerText--;
+    } else if (timeDisplay.innerText === "0") {
+      isRunning = false;
+      clearInterval(countDownTimer);
+      startGame.disabled = false
     }
-    }, 1000);
-}
+  }, 1000);
+};
 
-
-target.addEventListener("click", () =>{
-    if (isRunning){
-        movingTarget()
-        scoreDisplay.innerText ++
-        increaseDifficulty()
-        }
-})
-
+target.addEventListener("click", () => {
+  if (isRunning) {
+    movingTarget();
+    scoreDisplay.innerText++;
+    increaseDifficulty();
+  }
+});
